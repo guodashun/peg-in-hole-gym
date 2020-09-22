96a8cfc6072442f88edaa949a7a409408b147d80
luckky <guodashun@163.com>
2020-09-22 20:14:10 +0800

    reconstracted hole model

--- env.py
+++ env.py
@@ -8,11 +8,11 @@ def create_env():
     pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "franka_panda/panda.urdf"),useFixedBase=True)
     tableUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "table/table.urdf"),basePosition=[0.5,0,-0.65])
     # trayUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "tray/traybox.urdf"),basePosition=[0.65,0,0])
-    boxUid = p.loadURDF("./urdf/insertion_box.urdf",basePosition=[0.5,0,0])
-    objectSize = [0.01, 0.01, 0.2]
+    boxUid = p.loadURDF("./urdf/square_20mm.urdf",basePosition=[0.3,0.3,0])
+    objectSize = [0.01, 0.01, 0.1]
     objectUidC = p.createCollisionShape(p.GEOM_BOX, halfExtents=objectSize)
     objectUidV = p.createCollisionShape(p.GEOM_BOX, halfExtents=objectSize)
-    objectUid = p.createMultiBody(0.1, objectUidC, objectUidV , basePosition = [0.55, -0.04, 1])
+    objectUid = p.createMultiBody(0.1, objectUidC, objectUidV , basePosition = [0.59, -0.02, 1])
     while True:
         p.stepSimulation()
 
