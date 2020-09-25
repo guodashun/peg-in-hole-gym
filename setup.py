ec5c604fadbaadcae0fc02292e83c9ec443c0296
luckky <guodashun@163.com>
2020-09-25 21:47:29 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='peg_in_hole_rl',
+    version='0.0.1',
+    description='An gym env for simulating flexible tube grasp.',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(include=''),
+    install_requires=['gym', 'pybullet'],
+    url='https://github.com/guodashun/peg_in_hole_rl',
+    author='luckky',
+    author_email='luckky@gmail.com',
+    license='MIT',
+)
