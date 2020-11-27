c274f298744fccd9125f861362c7650161c062e0
luckky <guodashun@163.com>
2020-11-27 18:43:17 +0800

    finish multiprocessing

--- peg_in_hole_gym/envs/panda_env_mp.py
+++ peg_in_hole_gym/envs/panda_env_mp.py
@@ -1,39 +1,87 @@
-from multiprocessing.connection import Client
-from os import pardir
-from .panda_env import PandaEnv
 import pybullet as p
-import multiprocessing as mp
-# from pybullet_utils.bullet_client import BulletClient
-from multiprocessing import Pipe, Process
+import gym
+from .panda_env import PandaEnv
+from .utils import MPMultiAgentActionSpace, MPMultiAgentObservationSpace
+from multiprocessing import Queue, Process
 
-# from .panda_env import PandaEnv
+class PandaEnvMp(gym.Env):
 
-class PandaEnvMp(PandaEnv):
-    def __init__(self, client, task, is_test, mp_num):
+    CLOSE = 0
+    RESET = 1
+    STEP = 2
+    RENDER = 3
+    
+    def __init__(self, client, task, mp_num=1, sub_num=1, offset=[0,0,0], is_test=False):
         if client == p.GUI:
-            self.mp_num = 1
-        else:
-            self.mp_num = mp_num
+            print("Warning! GUI mode is not supported in this env.")
+            print("Auto switch to DIRECT mode.")
+            client = p.DIRECT
+        self.mp_num = mp_num
+        self.task = task
+        self.sub_num = sub_num
         self.clients = []
-        self.child_pipes = []
-        self.parent_pipes = []
+        self.msg_queues = []
+        self.res_queues = []
+        self.processes = []
         for i in range(self.mp_num):    
-            self.clients.append(PandaEnv(client, task, is_test))
-            parent_pipe, child_pipe = Pipe()
-            self.child_pipes.append(child_pipe)
-            self.parent_pipes.append(parent_pipe)
-        
-        # super(PandaEnvMp, self).__init__(client, task, is_test)
-        
+            self.clients.append(PandaEnv(client, task, self.sub_num, offset, is_test))
+            msg_queue = Queue(1)
+            res_queue = Queue(1)
+            self.msg_queues.append(msg_queue)
+            self.res_queues.append(res_queue)
+            sub_process = Process(target=self.worker, args=(i, self.msg_queues[i], self.res_queues[i]))
+            sub_process.start()
+            self.processes.append(sub_process)
+        self.action_space = MPMultiAgentActionSpace([self.clients[i].action_space for i in range(self.mp_num)])
+        self.observation_space = MPMultiAgentObservationSpace([self.clients[i].observation_space for i in range(self.mp_num)])
 
 
-    def step(self):
-        pass
+    def step(self, action):
+        for i in range(self.mp_num):
+            if not self.dones[i]:
+                self.msg_queues[i].put([self.STEP, action[i]])
+        for i in range(self.mp_num):
+            if not self.dones[i]:
+                obs, reward, done, info = self.res_queues[i].get()
+                self.observations[i] = obs
+                self.rewards[i] = reward
+                self.dones[i] = done
+                self.infos[i] = info
+        all_done = all(self.dones)
+        return self.observations, self.rewards, all_done, self.infos
 
     
     def reset(self):
-        pass
+        self.observations = self.observation_space.sample()
+        self.rewards = [[] for _ in range(self.mp_num)]
+        self.infos = [[] for _ in range(self.mp_num)]
+        self.dones = [False for _ in range(self.mp_num)]
+        for i in range(self.mp_num):
+            self.msg_queues[i].put([self.RESET, None])
+        for i in range(self.mp_num):
+            self.observations[i] = self.res_queues[i].get()
+        return self.observations
 
 
-    def render(self):
-        pass 
+    def render(self, mode):
+        for i in range(self.mp_num):
+            self.msg_queues[i].put([self.RENDER, mode])
+
+    def close(self):
+        for i in range(self.mp_num):
+            self.msg_queues[i].put([self.CLOSE, None])
+    
+
+    def worker(self, idx, msg_queue, res_queue):
+        while True:
+            msg, args = msg_queue.get()
+            if msg == self.RESET:
+                obs = self.clients[idx].reset()
+                res_queue.put(obs)
+            if msg == self.STEP:
+                obs, reward, done, info = self.clients[idx].step(args)
+                res_queue.put([obs, reward, done, info])
+            if msg == self.RENDER:
+                self.clients[idx].render(args)
+            if msg == self.CLOSE:
+                break
--- peg_in_hole_gym/envs/utils.py
+++ peg_in_hole_gym/envs/utils.py
@@ -81,3 +81,37 @@ class MultiAgentActionSpace(list):
     def sample(self):
         """ samples action for each agent from uniform distribution"""
         return [agent_action_space.sample() for agent_action_space in self._agents_action_space]
+
+
+class MPMultiAgentObservationSpace(MultiAgentObservationSpace):
+    def __init__(self, agents_observation_space):
+        for x in agents_observation_space:
+            assert isinstance(x, MultiAgentObservationSpace)
+
+        # super().__init__(agents_observation_space)
+        self._agents_observation_space = agents_observation_space
+
+    def sample(self):
+        """ samples observations for each agent from uniform distribution"""
+        return [agent_observation_space.sample() for agent_observation_space in self._agents_observation_space]
+
+    def contains(self, obs):
+        """ contains observation """
+        for space, ob in zip(self._agents_observation_space, obs):
+            if not space.contains(ob):
+                return False
+        else:
+            return True
+
+
+class MPMultiAgentActionSpace(MultiAgentActionSpace):
+    def __init__(self, agents_action_space):
+        for x in agents_action_space:
+            assert isinstance(x, MultiAgentActionSpace)
+
+        # super(MultiAgentActionSpace, self).__init__(agents_action_space)
+        self._agents_action_space = agents_action_space
+
+    def sample(self):
+        """ samples action for each agent from uniform distribution"""
+        return [agent_action_space.sample() for agent_action_space in self._agents_action_space]
